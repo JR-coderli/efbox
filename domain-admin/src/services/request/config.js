3cc699d454db9c050df447cbb4d9294b54b63377
JR <15898514+efjr@user.noreply.gitee.com>
2026-06-10 20:14:10 +0800

    add fronted .env

--- domain-admin/src/services/request/config.js
+++ domain-admin/src/services/request/config.js
@@ -1,56 +1,26 @@
-
-
-
-
 export const TIMEOUT = 10000
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const ENV = 'dev' // 'dev' | 'prod'
+const ENV = import.meta.env.VITE_ENV || 'dev'
 
 export const BASE_URL =
   ENV === 'prod'
-    ? 'https://efbox.work/api'
-    : 'http://localhost:8001'
+    ? import.meta.env.VITE_BASE_URL_PROD || 'https://efbox.work/api'
+    : import.meta.env.VITE_BASE_URL_DEV || 'http://localhost:8001'
 
 export const LP_API_URL =
-  ENV == 'prod'
-    ? 'http://8.134.108.134:8001'
-    : 'http://localhost:8002'
+  ENV === 'prod'
+    ? import.meta.env.VITE_LP_API_URL_PROD || 'http://8.134.108.134:8001'
+    : import.meta.env.VITE_LP_API_URL_DEV || 'http://localhost:8002'
 
 export const WS_CONFIG = {
   url:
     ENV === 'prod'
-      ? 'https://efbox.work'
-      : 'http://localhost:8001',
+      ? import.meta.env.VITE_WS_URL_PROD || 'https://efbox.work'
+      : import.meta.env.VITE_WS_URL_DEV || 'http://localhost:8001',
 
   path:
     ENV === 'prod'
-      ? '/api/socket.io'
-      : '/socket.io'
+      ? import.meta.env.VITE_WS_PATH_PROD || '/api/socket.io'
+      : import.meta.env.VITE_WS_PATH_DEV || '/socket.io'
 }
 
