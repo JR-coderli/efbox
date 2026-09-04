6acb5b1538da40514f599c4e4b693e9963cdd8af
lyp <2237962884@qq.com>
2026-09-04 16:18:40 +0800

    fix:错误日志-列设置

--- domain-admin/src/views/main/ef-tracker/error-logs/error-logs.vue
+++ domain-admin/src/views/main/ef-tracker/error-logs/error-logs.vue
@@ -70,8 +70,6 @@
                 </ul>
               </div>
             </el-popover>
-            <button class="google-btn google-btn-primary" @click="handleSearch">搜索</button>
-            <button class="google-btn google-btn-secondary" @click="handleReset">重置</button>
             <button class="google-btn google-btn-secondary" @click="handleRefresh" :disabled="loading || refreshCountdown > 0">
               <svg class="btn-icon" :class="{ 'is-loading': loading }" viewBox="0 0 24 24">
                 <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/>
@@ -257,18 +255,6 @@ async function loadData() {
 }
 
 function handleSearch() {
-  pagination.page = 1
-  loadData()
-}
-
-function handleReset() {
-  filters.keyword = ''
-  filters.mid = ''
-  filters.tid = ''
-  filters.http_status = ''
-  filters.exclude_error_code = 'SYSTEM_CLICK_FORMAT_ERR' // 恢复默认排除
-  dateRange.value = []
-  tz.value = 8
   pagination.page = 1
   loadData()
 }
